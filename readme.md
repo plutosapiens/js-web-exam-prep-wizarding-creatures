38c7af63fb39ce029b92678aa57b7b920073d570
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-10-17 21:07:04 +0300

    WIZ-1 Config view engine, add main layout

--- readme.md
+++ readme.md
@@ -3,3 +3,10 @@
 3 Install express and nodemon
     3.1 Configure static middleware
     3.2 Configure bodyparser
+    3.3 Configure routers
+4 Add images and css in public directory
+5 Add html files in view directory
+6 Install express-handlebars
+    6.1 Configure view engine
+    6.2 Add main layout
+    6.3 fix hyperlink styles
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 //ENTRY
 const express = require('express');
+const handlebars = require('express-handlebars');
 const path = require('path');
-const { PORT } = require('./constants')
+
+const { PORT } = require('./constants');
+//const routes = require('./router');
 
 //Init
 const app = express();
@@ -10,8 +13,16 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, './public')));
 app.use(express.urlencoded({ extended: false }));
 
+//Handlebars configuration
+app.engine('hbs', handlebars.engine({ extname: 'hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+//Routes
 app.get('/', (req, res) => {
-    res.send('Hello home page! :)');
+    res.render('layouts/main')
 });
+
+///app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
